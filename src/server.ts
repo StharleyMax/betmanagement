558d067f7c95bab10374f5871aa922c75a56eb13
Stharley Maxwell <stharleymax@gmail.com>
2022-03-10 09:31:36 -0300

    config dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Estou aqui e isso vai funcionar');
 });
 
-app.listen(port, () => {
-  console.log(`server is listenin on ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server is listenin on ${process.env.PORT}`);
 });
