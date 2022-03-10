268d8419294a5bffb767f13a3221e4669a19361e
Marcio Carolino <marciocarolino@gmail.com>
2022-03-09 21:18:18 -0300

    Projeto começando a ter vida, para executar para executar...     npm run start:dev

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Estou aqui e isso vai funcionar')
+});
+
+app.listen(port, () => {
+    console.log(`server is listenin on ${port}`)
+})
