57da1b13fcebd17b6c106fdd1d84c98a34618cbc
Stharley Maxwell <stharleymax@gmail.com>
2022-03-10 08:59:01 -0300

    initial config eslint + prittier and tsconfig

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 
-
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Estou aqui e isso vai funcionar')
+  res.send('Estou aqui e isso vai funcionar');
 });
 
 app.listen(port, () => {
-    console.log(`server is listenin on ${port}`)
-})
+  console.log(`server is listenin on ${port}`);
+});
