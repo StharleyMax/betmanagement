7bf9d1015594194ea8c15e9585785a54597d9930
Stharley Maxwell <stharleymax@gmail.com>
2022-03-14 10:22:28 -0300

    add celebration validade

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.get('/', usersController.index);
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      telephone: Joi.string().required(),
+      password: Joi.string().required(),
+      confirmPassword: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  usersController.create,
+);
 
 export default usersRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,11 +3,14 @@ import express from 'express';
 import 'reflect-metadata';
 import '@shared/typeorm';
 import routes from '@shared/http/routes';
+import { errors } from 'celebrate';
+import 'express-async-errors'
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errors);
 
 app.listen(process.env.PORT, () => {
   console.log(`server is listenin on ${process.env.PORT}`);
