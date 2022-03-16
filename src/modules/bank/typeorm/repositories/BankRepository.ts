e503f66f9f09f738b71b3dbf7d9fe9102e956abd
Stharley Maxwell <stharleymax@gmail.com>
2022-03-16 14:26:11 -0300

    fix findByUser

--- src/modules/bank/typeorm/repositories/BankRepository.ts
+++ src/modules/bank/typeorm/repositories/BankRepository.ts
@@ -4,7 +4,7 @@ import { Bank } from '../entities/Bank';
 @EntityRepository(Bank)
 export class BankRepository extends Repository<Bank> {
   public async findByUser(userId: number): Promise<Bank[] | undefined> {
-    const banks = await this.find();
+    const banks = await this.find({ userId });
     return banks;
   }
 
